bfc1c7c139681e200bd95e37408a6e5e72cf99e6
lukemandavid4 <lukemandavid4@gmail.com>
2024-06-14 18:43:00 +0300

    updated

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,8 +9,8 @@ import { chefs, People } from "../ui/data/data";
 const page = () => {
   return (
     <>
-      <div className="h-[27.25rem] bg-[url('/about/background-jainery.png')] bg-[var(--color-five)] bg-cover px-lr-custom flex items-center justify-center relative">
-        <h1 className="text-[var(--color-two)] text-[4.69rem] font-bold">About Us</h1>
+      <div className="min-h-[27.25rem] bg-[url('/about/background-jainery.png')] bg-[var(--color-five)] bg-cover px-[2rem] md:px-lr-custom flex items-center justify-center relative">
+        <h1 className="text-[var(--color-two)] text-[3.75rem] md:text-[4.69rem] font-bold">About Us</h1>
         <div className="flex px-[1rem] py-[0.5rem] bg-[var(--color-three)] rounded-[50vw] gap-[0.4rem] font-medium absolute bottom-[-1rem]">
           <Link href='/' className="hover:text-[var(--color-two)] text-[var(--color-white)] [transition:color_0.3s]">Home</Link>
           <span className="text-[var(--color-white)]">&#47;&#47;  About Us</span>
--- app/ui/navbar/Navbar.tsx
+++ app/ui/navbar/Navbar.tsx
@@ -9,7 +9,10 @@ import { useState } from "react";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false)
-  const handleClick = () =>{
+  const handleOpen = () =>{
+    setOpen(!open)
+  }
+  const handleClose = () =>{
     setOpen(!open)
   }
   return (
@@ -18,7 +21,7 @@ const Navbar = () => {
         <div>
           <Link href='/'><Image src='/home/logo.png' width={125} height={50} alt="logo"></Image></Link>
         </div>
-        <div className={`gap-[2.5rem] font-medium text-[1.125rem] text-[var(--color-one)] absolute bg-[#f9e3d0] flex flex-col items-center top-0 left-[-100%] pt-[5.5rem] w-[100vw] h-[100dvh] md:static md:flex ${open ? 'opacity-0 pointer-events-none': ''}`}>
+        <div className={`gap-[2.5rem] font-medium text-[1.125rem] text-[var(--color-one)] absolute bg-[#f9e3d0] md:bg-[var(--color-white)] flex flex-col items-center top-0 w-[100vw] pt-[5.5rem] md:pt-0 md:w-auto md:flex-row h-[100dvh] md:h-auto md:static md:flex [transition:left_0.3s] ${open ? 'left-[0]': 'left-[-100%]'}`}>
           <Link href='/' className="focus:text-[var(--color-three)] hover:text-[var(--color-three)] [transition:color_0.3s]">Home</Link>
           <Link href='/about' className="focus:text-[var(--color-three)] hover:text-[var(--color-three)] [transition:color_0.3s]">About</Link>
           <Link href='/shop' className="focus:text-[var(--color-three)] hover:text-[var(--color-three)] [transition:color_0.3s]">Products</Link>
@@ -33,14 +36,14 @@ const Navbar = () => {
             </div>
           </div>
           <Link href='/contact' className="focus:text-[var(--color-three)] hover:text-[var(--color-three)] [transition:color_0.3s]">Contact</Link>
-          <div className="p-[0.5rem] bg-[var(--color-two)] rounded-[50%] cursor-pointer absolute top-[2rem] right-[2rem]">
-          <IoClose className="text-[1.5rem] text-[var(--color-white)]" onClick={handleClick}/>
+          <div className="p-[0.5rem] bg-[var(--color-two)] rounded-[50%] cursor-pointer absolute top-[2rem] right-[2rem] md:hidden" onClick={handleClose}>
+            <IoClose className="text-[1.5rem] text-[var(--color-white)]"/>
           </div>
         </div>
         <div className="flex items-center gap-[2rem]">
           <button className="border-[1px] p-[0.5rem] border-[var(--color-grey)] rounded-[50%]"><TiShoppingCart  className="text-[1.5rem]"/></button>
           <Link href='/contact' className="bg-[var(--color-three)] text-[var(--color-white)] px-[2rem] py-[1rem] rounded-[50vw] font-medium text-[1.1875rem] hover:bg-[var(--color-two)] [transition:background_0.3s] hidden md:flex">Book Reservartion</Link>
-          <div className="bg-[var(--color-two)] p-[0.4rem] rounded cursor-pointer flex md:hidden">
+          <div className="bg-[var(--color-two)] p-[0.4rem] rounded cursor-pointer flex md:hidden" onClick={handleOpen}>
             <CgMenuRightAlt className="text-[1.5rem] text-[var(--color-white)]"/>
           </div>
         </div>
