900b72b363a1ebcc9081e64b5338314e46bba593
lukemandavid4 <lukemandavid4@gmail.com>
2024-07-16 11:53:30 +0300

    updated

--- app/ui/cart/cart.tsx
+++ app/ui/cart/cart.tsx
@@ -1,13 +1,28 @@
 import { IoClose } from "react-icons/io5";
+import Link from "next/link";
 
 const Cart = () => {
   return (
-    <main className="fixed bg-white right-0 top-0 w-[30rem] h-full p-6">
-      <div className="flex">
+    <main className="fixed bg-white right-0 top-0 w-[30rem] h-full py-6">
+      <div className="flex items-center justify-between px-6">
         <h1 className="text-[1.4375rem] font-bold text-[var(--color-two)]">
           Your Cart
         </h1>
-        <IoClose />
+        <IoClose className="text-[1.8rem] cursor-pointer text-[var(--color-two)]" />
+      </div>
+      <hr className="mt-3" />
+      <div className="px-6 h-full">Hello</div>
+      <div className="px-6 absolute bottom-5 w-full flex flex-col gap-4">
+        <div className="flex justify-between">
+          <span className="text-[var(--color-four)]">Subtotal</span>
+          <span className="font-bold text-[var(--color-four)]">Ksh. 250</span>
+        </div>
+        <Link
+          href="/shop"
+          className="bg-[var(--color-three)] flex px-[2rem] md:px-[3rem] rounded-[50vw] items-center justify-center font-medium gap-[1rem] py-[0.8rem] text-[var(--color-white)] hover:bg-[var(--color-two)] [transition:background_0.3s] self-stretch"
+        >
+          Continue To Checkout
+        </Link>
       </div>
     </main>
   );
