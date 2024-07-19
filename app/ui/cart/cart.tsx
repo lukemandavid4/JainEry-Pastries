1a9fe6d529be219d850f0de11f4da7b005366461
lukemandavid4 <lukemandavid4@gmail.com>
2024-07-19 15:59:41 +0300

    updated

--- app/ui/cart/cart.tsx
+++ app/ui/cart/cart.tsx
@@ -2,8 +2,9 @@
 import { IoClose } from "react-icons/io5";
 import Link from "next/link";
 import { useState } from "react";
+
 type CartProps = {
-  otherStyles: any;
+  otherStyles: string; // Specify the type more accurately
 };
 
 const Cart: React.FC<CartProps> = ({ otherStyles }) => {
@@ -11,11 +12,12 @@ const Cart: React.FC<CartProps> = ({ otherStyles }) => {
   const handleClick = () => {
     setClose(!close);
   };
+
   return (
     <main
-      className={`fixed bg-white  ${otherStyles} top-0 w-full 2sm:w-[30rem] h-full py-6 z-[10000] ${
+      className={`fixed bg-white ${otherStyles} top-0 w-full 2sm:w-[30rem] h-full py-6 z-[10000] ${
         close ? "right-[-100%]" : "right-0"
-      } [transition:right_,0.3s] `}
+      } transition-right duration-300 ease-in-out`}
     >
       <div className="flex items-center justify-between px-6">
         <h1 className="text-[1.4375rem] font-bold text-[var(--color-two)]">
@@ -24,6 +26,7 @@ const Cart: React.FC<CartProps> = ({ otherStyles }) => {
         <IoClose
           className="text-[1.8rem] cursor-pointer text-[var(--color-two)]"
           onClick={handleClick}
+          aria-label="Close cart"
         />
       </div>
       <hr className="mt-3" />
@@ -36,7 +39,7 @@ const Cart: React.FC<CartProps> = ({ otherStyles }) => {
         </div>
         <Link
           href="/shop"
-          className="bg-[var(--color-three)] flex px-[2rem] md:px-[3rem] rounded-[50vw] items-center justify-center font-medium gap-[1rem] py-[0.8rem] text-[var(--color-white)] hover:bg-[var(--color-two)] [transition:background_0.3s] self-stretch"
+          className="bg-[var(--color-three)] flex px-[2rem] md:px-[3rem] rounded-[50vw] items-center justify-center font-medium gap-[1rem] py-[0.8rem] text-[var(--color-white)] hover:bg-[var(--color-two)] transition-colors duration-300 self-stretch"
         >
           Continue To Checkout
         </Link>
--- app/ui/navbar/Navbar.tsx
+++ app/ui/navbar/Navbar.tsx
@@ -11,79 +11,59 @@ import Cart from "../cart/cart";
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   const [openCart, setOpenCart] = useState(false);
+
   const handleOpenCart = () => {
     setOpenCart(!openCart);
   };
-  const handleOpen = () => {
+
+  const handleToggleMenu = () => {
     setOpen(!open);
   };
-  const handleClose = () => {
-    setOpen(!open);
-  };
+
   return (
     <>
       <div className="h-[5.5rem] px-[2rem] xl:px-lr-custom bg-[var(--color-white)] flex items-center justify-between relative lg:static">
         <div>
           <Link href="/">
-            <Image
-              src="/home/logo.png"
-              width={125}
-              height={50}
-              alt="logo"
-            ></Image>
+            <Image src="/home/logo.png" width={125} height={50} alt="logo" />
           </Link>
         </div>
-        <div
-          className={`gap-[2.5rem] font-medium text-[1.1rem] text-[var(--color-one)] fixed bg-[#f9e3d0] lg:bg-[var(--color-white)] flex flex-col pl-[2rem] top-0 w-[75vw] pt-[5.5rem] lg:pt-0 lg:w-auto lg:flex-row h-full lg:h-auto lg:static [transition:left_0.3s] ${
-            open ? "left-[0] fixed" : "left-[-110%]"
+        <nav
+          className={`gap-[2.5rem] font-medium text-[1.1rem] text-[var(--color-one)] fixed bg-[#f9e3d0] lg:bg-[var(--color-white)] flex flex-col pl-[2rem] top-0 w-[75vw] pt-[5.5rem] lg:pt-0 lg:w-auto lg:flex-row h-full lg:h-auto lg:static transition-left duration-300 ${
+            open ? "left-0 fixed" : "left-[-110%]"
           } z-[999]`}
         >
-          <Link
-            href="/"
-            className="focus:text-[var(--color-three)] hover:text-[var(--color-three)] [transition:color_0.3s]"
-            onClick={handleClose}
-          >
-            Home
-          </Link>
-          <Link
-            href="/about"
-            className="focus:text-[var(--color-three)] hover:text-[var(--color-three)] [transition:color_0.3s]"
-            onClick={handleClose}
-          >
-            About
-          </Link>
-          <Link
-            href="/shop"
-            className="focus:text-[var(--color-three)] hover:text-[var(--color-three)] [transition:color_0.3s]"
-            onClick={handleClose}
-          >
-            Products
-          </Link>
-          <Link
-            href="/blog"
-            className="focus:text-[var(--color-three)] hover:text-[var(--color-three)] [transition:color_0.3s]"
-            onClick={handleClose}
-          >
-            Blogs
-          </Link>
-          <div>
-            <div
-              className="cursor-pointer focus:text-[var(--color-three)]"
-              onClick={handleOpen}
+          {[
+            { href: "/", label: "Home" },
+            { href: "/about", label: "About" },
+            { href: "/shop", label: "Products" },
+            { href: "/blog", label: "Blogs" },
+            { href: "/contact", label: "Contact" },
+          ].map((link) => (
+            <Link
+              key={link.href}
+              href={link.href}
+              className="focus:text-[var(--color-three)] hover:text-[var(--color-three)] transition-colors duration-300"
             >
-              <button className=" flex items-center gap-[0.3rem] hover:text-[var(--color-three)] [transition:color_0.3s]">
-                Pages <IoIosArrowDown />
-              </button>
-            </div>
+              {link.label}
+            </Link>
+          ))}
+          <div>
+            <button
+              className="cursor-pointer focus:text-[var(--color-three)] flex items-center gap-[0.3rem] hover:text-[var(--color-three)] transition-colors duration-300"
+              onClick={handleToggleMenu}
+            >
+              Pages <IoIosArrowDown />
+            </button>
             <div
-              className={`flex-col hidden text-[var(--color-two)] rounded-md justify-center md:items-center absolute md:bg-[var(--color-five)] w-[8rem] h-[5rem] ${
+              className={`flex-col text-[var(--color-two)] rounded-md justify-center md:items-center absolute md:bg-[var(--color-five)] w-[8rem] h-[5rem] ${
                 open ? "flex" : "hidden"
               }`}
             >
               <Link
                 href="/faq"
                 className="focus:text-[var(--color-three)] hover:text-[var(--color-three)]"
-                onClick={handleClose}
+                onClick={handleToggleMenu}
               >
                 FAQ
               </Link>
@@ -91,37 +71,31 @@ const Navbar = () => {
               <Link
                 href="/not-found"
                 className="focus:text-[var(--color-three)] hover:text-[var(--color-three)]"
-                onClick={handleClose}
+                onClick={handleToggleMenu}
               >
                 404
               </Link>
               <div className="w-full h-[0.1rem] absolute bg-[var(--color-three)] bottom-0"></div>
             </div>
           </div>
-          <Link
-            href="/contact"
-            className="focus:text-[var(--color-three)] hover:text-[var(--color-three)] [transition:color_0.3s]"
-            onClick={handleClose}
-          >
-            Contact
-          </Link>
-        </div>
+        </nav>
         <div className="flex items-center gap-[2rem] xl:gap-4">
           <button
             className="border-[1px] p-[0.5rem] border-[var(--color-grey)] rounded-[50%]"
             onClick={handleOpenCart}
+            aria-label="Toggle cart"
           >
             <TiShoppingCart className="text-[1.5rem]" />
           </button>
           <Link
             href="/contact"
-            className="bg-[var(--color-three)] text-[var(--color-white)] px-[2rem] py-[1rem] rounded-[50vw] font-medium text-[1rem] hover:bg-[var(--color-two)] [transition:background_0.3s] hidden lg:flex"
+            className="bg-[var(--color-three)] text-[var(--color-white)] px-[2rem] py-[1rem] rounded-[50vw] font-medium text-[1rem] hover:bg-[var(--color-two)] transition-colors duration-300 hidden lg:flex"
           >
-            Book Reservartion
+            Book Reservation
           </Link>
           <div
             className="bg-[var(--color-two)] p-[0.4rem] rounded cursor-pointer flex lg:hidden"
-            onClick={handleOpen}
+            onClick={handleToggleMenu}
           >
             <CgMenuRightAlt
               className={`text-[1.5rem] text-[var(--color-white)] ${
