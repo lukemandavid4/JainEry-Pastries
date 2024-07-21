3c6e271853a0ec032aa51959704a4d0e65dc329d
lukemandavid4 <lukemandavid4@gmail.com>
2024-07-21 13:09:39 +0300

    updated

--- app/ui/navbar/Navbar.tsx
+++ app/ui/navbar/Navbar.tsx
@@ -6,7 +6,6 @@ import { CgMenuRightAlt } from "react-icons/cg";
 import { TiShoppingCart } from "react-icons/ti";
 import { IoClose } from "react-icons/io5";
 import { useState } from "react";
-import Cart from "../cart/cart";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -80,7 +79,37 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-      <Cart otherStyles={`${openCart ? "right-0" : "right-[-100%]"}`} />
+      <div
+        className={`fixed bg-white top-0 w-full 2sm:w-[30rem] h-full py-6 z-[10000] ${
+          openCart ? "right-0" : "right-[-100%]"
+        } transition-right duration-300 ease-in-out`}
+      >
+        <div className="flex items-center justify-between px-6">
+          <h1 className="text-[1.4375rem] font-bold text-[var(--color-two)]">
+            Your Cart
+          </h1>
+          <IoClose
+            className="text-[1.8rem] cursor-pointer text-[var(--color-two)]"
+            aria-label="Close cart"
+            onClick={handleOpenCart}
+          />
+        </div>
+        <hr className="mt-3" />
+        <div className="px-6 h-full">Hello</div>
+        <hr />
+        <div className="px-6 absolute bottom-5 w-full flex flex-col gap-4">
+          <div className="flex justify-between">
+            <span className="text-[var(--color-four)]">Subtotal</span>
+            <span className="font-bold text-[var(--color-four)]">Ksh. 250</span>
+          </div>
+          <Link
+            href="/shop"
+            className="bg-[var(--color-three)] flex px-[2rem] md:px-[3rem] rounded-[50vw] items-center justify-center font-medium gap-[1rem] py-[0.8rem] text-[var(--color-white)] hover:bg-[var(--color-two)] transition-colors duration-300 self-stretch"
+          >
+            Continue To Checkout
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
